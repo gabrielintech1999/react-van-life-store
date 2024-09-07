24608c9060b99eab383809ac15f3a5d6fc00bce9
Gabriel Gomes <gabrielgomesdeveloper1999@gmail.com>
2024-09-07 06:53:50 +0100

    setting up the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
+function Home() {
+  return <div>Home Page</div>;
+}
+
+function About() {
+  return <div>About Page</div>;
+}
+
+function NavBar() {
+  return (
+    <nav>
+      <h1>
+        {" "}
+        <Link to="/">VanLife</Link>
+      </h1>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </ul>
+    </nav>
+  );
+}
 export default function App() {
   return (
-    <div>
-      Hello SaSS
-    </div>
-  )
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
