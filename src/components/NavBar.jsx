192213467c464d94c829be8b34b68475fdb8ae7e
Gabriel Gomes <gabrielgomesdeveloper1999@gmail.com>
2024-09-07 12:37:09 +0100

    adding files

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+    return (
+      <nav>
+        <h1>
+          {" "}
+          <Link to="/">VanLife</Link>
+        </h1>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </ul>
+      </nav>
+    );
+  }
